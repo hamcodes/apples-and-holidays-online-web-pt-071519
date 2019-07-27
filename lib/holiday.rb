12669d024011c3abce7230976f5cbc53836df485
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-27 12:19:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 # end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter]# return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten# return an array of all of the supplies that are used in the winter season
 
 end
 
