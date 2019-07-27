5ac5d9067af56ef35222f3cc9ad3c06961c7f891
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-27 11:59:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
    items << "Grill"
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
