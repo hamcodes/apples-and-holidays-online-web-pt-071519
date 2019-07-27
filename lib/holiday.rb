4c248b2a132f0e6b76c2754808cdafe0221a9dde
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-27 12:06:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,7 @@ holiday_hash[:winter].each do |holiday, decorations|
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
-end
+
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
