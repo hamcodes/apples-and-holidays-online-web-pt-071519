fcf1332ee1ac65dac2fd35b6d90160dd7e93bc03
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-27 12:05:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,9 +24,9 @@ end
 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |holiday, decorations|
-    
+holiday_hash[:winter].each do |holiday, decorations|
    decorations << supply
+ end
  end
 
   # holiday_hash is identical to the one above
